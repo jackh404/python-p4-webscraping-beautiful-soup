d8f0725e5088c5650ee5acc5aca03eeccc137b0d
Jack Henza <jackhenza@gmail.com>
2023-12-25 14:30:09 +0000

    scrape

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,9 @@
-from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+h4s = doc.select('.heading-25-semibold.color-white')
+for h4 in h4s:
+    print(h4.contents[0].strip())
